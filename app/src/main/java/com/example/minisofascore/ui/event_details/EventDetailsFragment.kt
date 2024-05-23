0dd9a2868c3c965e19f073e5c81aee98dfd8bbb7
matkopapic <matko.papic@gmail.com>
2024-05-24 01:55:12 +0200

    Displaying Incidents in RecyclerView, only football layouts

--- app/src/main/java/com/example/minisofascore/ui/event_details/EventDetailsFragment.kt
+++ app/src/main/java/com/example/minisofascore/ui/event_details/EventDetailsFragment.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.minisofascore.data.models.Event
 import com.example.minisofascore.databinding.FragmentEventDetailsBinding
 import com.example.minisofascore.ui.main_list.EVENT_INFO
 
@@ -25,10 +26,20 @@ class EventDetailsFragment : Fragment() {
 
         _binding = FragmentEventDetailsBinding.inflate(inflater, container, false)
 
-        val event = requireArguments().getSerializable(EVENT_INFO)
+        val incidentAdapter = IncidentAdapter(requireContext())
 
-        val textView: TextView = binding.textNotifications
-        textView.text = event.toString()
+        binding.incidentRecyclerView.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = incidentAdapter
+        }
+
+        val event = (requireArguments().getSerializable(EVENT_INFO)) as Event
+
+        eventDetailsViewModel.getIncidents(1)
+        eventDetailsViewModel.incidents.observe(viewLifecycleOwner){
+            incidentAdapter.updateItems(it)
+        }
+
         return binding.root
     }
 
